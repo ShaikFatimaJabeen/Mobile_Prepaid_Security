397c1407a7d61b32e784bca7072de9f138aeb606
ShaikFatimaJabeen <shaikfatimajabeen@gmail.com>
2025-04-29 12:12:39 +0530

    Add files via upload

---
+++ mobileprepaid/src/main/java/com/aits/mobileprepaid/entity/RechargePlan.java
@@ -0,0 +1,40 @@
+package com.aits.mobileprepaid.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class RechargePlan {
+	
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	private String name;
+	private String category;
+	private double price;
+	
+	private int validityInDays;
+
+	public String getValidityInDays() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+	
+
+}
